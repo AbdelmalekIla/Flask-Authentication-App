663d04d36b9550076e8e277caca23b759268b800
AbdelmalekIla <115870374+AbdelmalekIla@users.noreply.github.com>
2023-12-13 15:35:36 +0100

    Update templates and routes: Display conditional content based on user authentication status in home, register, and base templates

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def load_user(user_id):
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/register', methods=["GET", "POST"])
@@ -50,7 +50,7 @@ def register():
             login_user(new_user)
             return redirect(url_for("secrets"))
 
-    return render_template("register.html")
+    return render_template("register.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -66,13 +66,13 @@ def login():
         else:
             flash('that email does not exits, please try again')
             return redirect(url_for('login'))
-    return render_template("login.html")
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/secrets')
 @login_required
 def secrets():
-    return render_template("secrets.html", name=current_user.name)
+    return render_template("secrets.html", name=current_user.name, logged_in=True)
 
 
 @app.route('/logout')
